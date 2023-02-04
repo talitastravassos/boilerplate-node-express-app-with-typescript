61fd4402e1339be4a268548d956dbce1c7f5a5e2
talitastravassos <talitatravassos.tt@gmail.com>
2023-02-04 10:31:41 -0300

    ADD: typescript, express and ts-node-dev

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+console.log("⚡ Server running ⚡");
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
+});
+
+app.listen(3333);
